a54f9fcf9c458fc5cc0ab612c72b254dc7762289
friendtu <friendtu@gmail.com>
2019-11-14 22:26:18 +0800

    the second way to mock

---
+++ c6/notesApp1Spec2.js
@@ -0,0 +1,18 @@
+describe('ItemCtrl wiht inline mock',function(){
+    beforeEach(module('notesApp1'));
+    var ctrl,mockService;
+    beforeEach(module({
+        ItemService:{
+            list:function() {
+                return [{id:1,label:'Mock'}];
+            }
+        }}));
+
+    beforeEach(inject(function($controller){
+        ctrl=$controller('ItemCtrl');
+    }));
+
+    it('should load mocked out items',function(){
+        expect(ctrl.items).toEqual([{id:1,label:'Mock'}]);
+    });
+});
