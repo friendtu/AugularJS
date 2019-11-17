8ba58d9f714bbc3b88b99ec0e445263519cbd68d
friendtu <friendtu@gmail.com>
2019-11-17 08:43:35 +0800

    use spy.calls.count()

--- c7/notesApp1SpecWithSpies.js
+++ c7/notesApp1SpecWithSpies.js
@@ -11,7 +11,7 @@ describe('ItemService with spy',function(){
     it('list should be called',function(){
         expect(itemService.list).toHaveBeenCalled();
         expect(itemService.list).toHaveBeenCalledTimes(1);
-        //expect(itemService.list.count()).toEqual(1);
+        expect(itemService.list.calls.count()).toEqual(1);
         expect(ctrl.items).toEqual([
             {id:1,label:'Item 0'},
             {id:2,label:'Item 1'}
