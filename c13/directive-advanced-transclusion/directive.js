9a2a16fe96114dc03a77b9673b3fcf6f4195553c
friendtu <friendtu@gmail.com>
2020-02-02 22:24:06 +0800

    advanced transclude

---
+++ c13/directive-advanced-transclusion/directive.js
@@ -0,0 +1,23 @@
+// File: chapter13/directive-no-transclusion/directive.js
+angular.module('stockMarketApp')
+  .directive('simpleStockRepeat', [function() {
+    return {
+      restrict:'A',
+      transclude: 'element',
+      link: function($scope, $element, $attrs,ctrl,$transclude) {
+        var myArray=$scope.$eval($attrs.simpleStockRepeat);
+
+        var container=angular.element('<div class="container"></div>');
+        for(var i=0;i<myArray.length;i++) {
+          var instance = $transclude($scope.$new(),
+            function(clonedElement,newScope){
+              newScope.currentIndex=i;
+              newScope.stock=myArray[i];
+            });
+            container.append(instance);
+        }
+
+        $element.after(container);
+      }
+    };
+  }]);
